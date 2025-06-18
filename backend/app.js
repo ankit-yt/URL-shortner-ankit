38b000a25e59cbf393b2434e22d2a2f936399aa6
ankit-yt <ankitpk3303@gmail.com>
2025-06-19 01:02:01 +0530

    updated core origin

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ import cookieParser from "cookie-parser";
 
 const app = express()
 app.use(cors({
-   origin:"http://localhost:5173",
+   origin:"https://url-shortner-ankit.vercel.app",
    credentials:true
 }))
 app.use(express.json())
